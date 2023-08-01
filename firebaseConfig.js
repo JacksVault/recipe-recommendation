57797d302a4e172e978a88e1b60d08067ca872cb
Jack Ojango <ojangoh2@outlook.com>
2023-08-01 17:20:55 +0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,12 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCSn9XcL0HxSFITzC8PaXGhPz7DkJTTYNs",
-  authDomain: "foodrecipe-recommendation.firebaseapp.com",
-  projectId: "foodrecipe-recommendation",
-  storageBucket: "foodrecipe-recommendation.appspot.com",
-  messagingSenderId: "775280490583",
-  appId: "1:775280490583:web:da30075eacf8de52ffcb12",
-  measurementId: "G-EKFW90HLVW"
+  
 };
 // Your backend code and application logic goes here
 
